48dfc630d2ecc0fb25541f0c2a0d1a10170696b1
Adnan Ahmad <adnan@eegamesstudio.com>
2017-03-17 16:39:29 +0500

    Changes added

---
+++ Example/Pods/Target Support Files/BDatePicker/BDatePicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BDatePicker : NSObject
+@end
+@implementation PodsDummy_BDatePicker
+@end
---
+++ Example/Pods/Target Support Files/Pods-BDatePicker_Tests/Pods-BDatePicker_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BDatePicker_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BDatePicker_Tests
+@end
